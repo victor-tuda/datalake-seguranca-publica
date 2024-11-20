35ae8ed7115d4004fc82780c422dedc7fa446f88
victor-tuda <victor-tuda>
2024-11-20 01:43:37 +0000

    feat: first commit

---
+++ src/bronze/Ingestão data-sus.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+catalog = dbutils.widgets.get("catalog")
+schema = dbutils.widgets.get("schema")
+table_name = dbutils.widgets.get("table_name")
+path = dbutils.widgets.get("path")
+sep = dbutils.widgets.get("sep")
+
+# COMMAND ----------
+
+df_data_sus = spark.read.csv(f"{path}/*.csv", sep=sep, header=True)
+
+# COMMAND ----------
+
+df_data_sus.count()
