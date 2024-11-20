f481853ad67304300a1f115519f99cccfbdf6311
victor-tuda <victor-tuda>
2024-11-20 19:02:05 +0000

    fix: nome das colunas

--- src/bronze/Ingestão ibge.py
+++ src/bronze/Ingestão ibge.py
@@ -14,4 +14,22 @@ df_municipios = (spark.read
 
 # COMMAND ----------
 
+# DBTITLE 1,Renomeando colunas
+df_municipios = (df_municipios
+    .withColumnRenamed('Nome_UF', 'nome_uf')
+    .withColumnRenamed('Região Geográfica Intermediária', 'codigo_regiao_geografica_intermediaria')
+    .withColumnRenamed('Nome Região Geográfica Intermediária', 'nome_regiao_geografica_intermediaria')
+    .withColumnRenamed('Região Geográfica Imediata', 'codigo_regiao_geografica_imediata')
+    .withColumnRenamed('Nome Região Geográfica Imediata', 'nome_regiao_geografica_imediata')
+    .withColumnRenamed('Mesorregião Geográfica', 'codigo_mesorregiao_geografica')
+    .withColumnRenamed('Nome_Mesorregião', 'nome_mesorregiao_geografica')
+    .withColumnRenamed('Microrregião Geográfica', 'codigo_microrregiao_geografica')
+    .withColumnRenamed('Nome_Microrregião', 'nome_microrregiao_geografica')
+    .withColumnRenamed('Município', 'codigo_municipio')
+    .withColumnRenamed('Código Município Completo', 'codigo_municipio_completo')
+    .withColumnRenamed('Nome_Município', 'nome_municipio')
+)
+
+# COMMAND ----------
+
 df_municipios.write.format('delta').mode('overwrite').saveAsTable(f'{catalog}.{schema}.{table_name}')
