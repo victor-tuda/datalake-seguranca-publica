712c2f470daf03dbf40a84728931be291d8ade11
victor-tuda <victor-tuda>
2024-12-26 18:56:17 +0000

    refactor

--- src/bronze/Ingestão fogo-cruzado.py
+++ src/bronze/Ingestão fogo-cruzado.py
@@ -8,40 +8,46 @@ sigla = table_name.upper()
 # COMMAND ----------
 
 # DBTITLE 1,df_fogo_cruzado formato json
-df_fogo_cruzado_json = spark.read.format("json").option("multiline", "true").load(f"/Volumes/raw/fogo-cruzado/s3-fogo-cruzado/{sigla}")
+df_raw = spark.read.format("json").option("multiline", "true").load(f"/Volumes/raw/fogo-cruzado/s3-fogo-cruzado/{sigla}")
+
+# COMMAND ----------
+
+# DBTITLE 1,Realizando o explode do conteúdo
+from pyspark.sql.functions import explode
+df_raw_exploded = df_raw.withColumn('explodedContent', explode('data')).drop('data')
 
 # COMMAND ----------
 
 # DBTITLE 1,Convertendo json para o formato tabular
-from pyspark.sql.functions import explode, col, explode_outer
+from pyspark.sql.functions import col, explode_outer
 
-df_fogo_cruzado = df_fogo_cruzado_json.withColumn('data1', explode('data')) \
-    .withColumn('id', col('data1.id')) \
-    .withColumn('documentNumber', col('data1.documentNumber')) \
-    .withColumn('date', col('data1.date')) \
-    .withColumn('agentPresence', col('data1.agentPresence')) \
-    .withColumn('address', col('data1.address')) \
-    .withColumn('latitude', col('data1.latitude')) \
-    .withColumn('longitude', col('data1.longitude')) \
-    .withColumn('relatedRecord', col('data1.relatedRecord')) \
+df_raw_with_columns = df_raw_exploded \
+    .withColumn('id', col('explodedContent.id')) \
+    .withColumn('documentNumber', col('explodedContent.documentNumber')) \
+    .withColumn('date', col('explodedContent.date')) \
+    .withColumn('agentPresence', col('explodedContent.agentPresence')) \
+    .withColumn('address', col('explodedContent.address')) \
+    .withColumn('latitude', col('explodedContent.latitude')) \
+    .withColumn('longitude', col('explodedContent.longitude')) \
+    .withColumn('relatedRecord', col('explodedContent.relatedRecord')) \
     \
-    .withColumn('locality_id', col('data1.locality.id')) \
-    .withColumn('locality_name', col('data1.locality.name')) \
+    .withColumn('locality_id', col('explodedContent.locality.id')) \
+    .withColumn('locality_name', col('explodedContent.locality.name')) \
     \
-    .withColumn('region_id', col('data1.region.id')) \
-    .withColumn('region_region', col('data1.region.region')) \
-    .withColumn('region_enabled', col('data1.region.enabled')) \
-    .withColumn('region_state', col('data1.region.state')) \
+    .withColumn('region_id', col('explodedContent.region.id')) \
+    .withColumn('region_region', col('explodedContent.region.region')) \
+    .withColumn('region_enabled', col('explodedContent.region.enabled')) \
+    .withColumn('region_state', col('explodedContent.region.state')) \
     \
-    .withColumn('state_id', col('data1.state.id')) \
-    .withColumn('state_name', col('data1.state.name')) \
+    .withColumn('state_id', col('explodedContent.state.id')) \
+    .withColumn('state_name', col('explodedContent.state.name')) \
     \
-    .withColumn('city_id', col('data1.city.id')) \
-    .withColumn('city_name', col('data1.city.name')) \
+    .withColumn('city_id', col('explodedContent.city.id')) \
+    .withColumn('city_name', col('explodedContent.city.name')) \
     \
     \
     \
-    .withColumn('victims1', explode_outer('data1.victims')) \
+    .withColumn('victims1', explode_outer('explodedContent.victims')) \
     .withColumn('victims_id', col('victims1.id')) \
     .withColumn('victims_occurrenceId', col('victims1.occurrenceId')) \
     .withColumn('victims_age', col('victims1.age')) \
@@ -93,25 +99,25 @@ df_fogo_cruzado = df_fogo_cruzado_json.withColumn('data1', explode('data')) \
     \
     \
     \
-    .withColumn('contextInfo_policeUnit', col('data1.contextInfo.policeUnit')) \
-    .withColumn('contextInfo_massacre', col('data1.contextInfo.massacre')) \
+    .withColumn('contextInfo_policeUnit', col('explodedContent.contextInfo.policeUnit')) \
+    .withColumn('contextInfo_massacre', col('explodedContent.contextInfo.massacre')) \
     \
-    .withColumn('contextInfo_mainReason_id', col('data1.contextInfo.mainReason.id')) \
-    .withColumn('contextInfo_mainReason_name', col('data1.contextInfo.mainReason.name')) \
+    .withColumn('contextInfo_mainReason_id', col('explodedContent.contextInfo.mainReason.id')) \
+    .withColumn('contextInfo_mainReason_name', col('explodedContent.contextInfo.mainReason.name')) \
     \
-    .withColumn('contextInfo_complementaryReasons_id', col('data1.contextInfo.complementaryReasons.id')) \
-    .withColumn('contextInfo_complementaryReasons_name', col('data1.contextInfo.complementaryReasons.name')) \
+    .withColumn('contextInfo_complementaryReasons_id', col('explodedContent.contextInfo.complementaryReasons.id')) \
+    .withColumn('contextInfo_complementaryReasons_name', col('explodedContent.contextInfo.complementaryReasons.name')) \
     \
-    .withColumn('contextInfo_clippings_id', col('data1.contextInfo.clippings.id')) \
-    .withColumn('contextInfo_clippings_name', col('data1.contextInfo.clippings.name')) \
+    .withColumn('contextInfo_clippings_id', col('explodedContent.contextInfo.clippings.id')) \
+    .withColumn('contextInfo_clippings_name', col('explodedContent.contextInfo.clippings.name')) \
     \
-    .withColumn('neighborhood_id', col('data1.neighborhood.id')) \
-    .withColumn('neighborhood_name', col('data1.neighborhood.name')) \
+    .withColumn('neighborhood_id', col('explodedContent.neighborhood.id')) \
+    .withColumn('neighborhood_name', col('explodedContent.neighborhood.name')) \
     \
-    .withColumn('subNeighborhood_id', col('data1.subNeighborhood.id')) \
-    .withColumn('subNeighborhood_name', col('data1.subNeighborhood.name')) \
+    .withColumn('subNeighborhood_id', col('explodedContent.subNeighborhood.id')) \
+    .withColumn('subNeighborhood_name', col('explodedContent.subNeighborhood.name')) \
     \
-    .withColumn('transports1', explode_outer('data1.transports')) \
+    .withColumn('transports1', explode_outer('explodedContent.transports')) \
     .withColumn('transports_id', col('transports1.id')) \
     .withColumn('transports_interruptedTransport', col('transports1.interruptedTransport')) \
     .withColumn('transports_dateInterruption', col('transports1.dateInterruption')) \
@@ -122,16 +128,16 @@ df_fogo_cruzado = df_fogo_cruzado_json.withColumn('data1', explode('data')) \
     .withColumn('transport_id', col('transports1.transport.id')) \
     .withColumn('transport_name', col('transports1.transport.name')) \
     \
-    .withColumn('animalVictims1', explode_outer('data1.animalVictims')) \
+    .withColumn('animalVictims1', explode_outer('explodedContent.animalVictims')) \
     .withColumn('animalVictims_id', col('animalVictims1.id')) \
     .withColumn('animalVictims_name', col('animalVictims1.name')) \
     .withColumn('animalVictims_occurrenceId', col('animalVictims1.occurrenceId')) \
     .withColumn('animalVictims_situation', col('animalVictims1.situation')) \
     .withColumn('animalVictims_type', col('animalVictims1.type')) \
     \
-    .drop('data', 'code', 'msg', 'msgCode', 'pageMeta', 'data1', 'victims1', 'transports1', 'animalVictims1') \
+    .drop('data', 'code', 'msg', 'msgCode', 'pageMeta', 'explodedContent', 'victims1', 'transports1', 'animalVictims1') \
     
 
 # COMMAND ----------
 
-df_fogo_cruzado.write.format('delta').mode('overwrite').saveAsTable(f'bronze.fogo_cruzado.{table_name}')
+df_raw_with_columns.write.format('delta').mode('overwrite').saveAsTable(f'bronze.fogo_cruzado.{table_name}')
