0fbf797cf6ee607301776d62ce12a3235daf1de7
victor-tuda <victor-tuda>
2024-12-26 17:38:12 +0000

    fix: ingestao

--- src/silver/Silver fogo-cruzado concatenado.py
+++ src/silver/Silver fogo-cruzado concatenado.py
@@ -1,20 +1,14 @@
 # Databricks notebook source
-# MAGIC %sql
-# MAGIC CREATE TABLE  IF NOT EXISTS silver.fogo_cruzado.rj_pe AS
-# MAGIC SELECT * FROM silver.fogo_cruzado.rj
-# MAGIC UNION ALL
-# MAGIC SELECT * FROM silver.fogo_cruzado.pe;
-# MAGIC
-
-# COMMAND ----------
-
 # Check if the table exists
 if spark.catalog.tableExists("silver.fogo_cruzado.rj_pe"):
-    spark.sql(f"""
-        CREATE TABLE  IF NOT EXISTS silver.fogo_cruzado.rj_pe AS
-        SELECT * FROM silver.fogo_cruzado.rj
-        UNION ALL
-        SELECT * FROM silver.fogo_cruzado.pe;
-    """)
+    df_fogo_cruzado_concatenado = spark.sql(f"""SELECT * FROM silver.fogo_cruzado.rj AS t1
+                                            FULL JOIN silver.fogo_cruzado.pe AS t2 ON t1.id = t2.id""")
+    df_fogo_cruzado_concatenado.write.format('delta').mode('overwrite').saveAsTable(f'silver.fogo_cruzado.rj_pe')
 else:
-    print("test")
+    spark.sql(f"""
+        CREATE TABLE silver.fogo_cruzado.rj_pe AS
+            SELECT * FROM silver.fogo_cruzado.rj
+            UNION ALL
+            SELECT * FROM silver.fogo_cruzado.pe;
+    """)
+
