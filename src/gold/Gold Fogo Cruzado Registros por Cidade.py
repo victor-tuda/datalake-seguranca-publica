16e2037ec2540d43aa47b4de8e92bba6aed13b86
victor-tuda <victor-tuda>
2025-05-29 14:21:54 +0000

    fix: changed column names

--- src/gold/Gold Fogo Cruzado Registros por Cidade.py
+++ src/gold/Gold Fogo Cruzado Registros por Cidade.py
@@ -1,7 +1,11 @@
 # Databricks notebook source
 # MAGIC %sql
 # MAGIC create table if not exists gold.fogo_cruzado.cidades_registros as
-# MAGIC select nome_cidade, count(*) as contagem_de_registros
+# MAGIC select municipio_nome, count(*) as contagem_de_registros
 # MAGIC from silver.fogo_cruzado.rj_pe
-# MAGIC group by nome_cidade
+# MAGIC group by municipio_nome
 # MAGIC order by contagem_de_registros desc
+
+# COMMAND ----------
+
+
