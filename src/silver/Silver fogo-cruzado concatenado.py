307bb42ff4c6483e055d00d399b0ff599d0a45a5
victor-tuda <victor-tuda>
2025-05-29 11:55:55 +0000

    fix: added except to pe dataset

--- src/silver/Silver fogo-cruzado concatenado.py
+++ src/silver/Silver fogo-cruzado concatenado.py
@@ -4,7 +4,7 @@ if spark.catalog.tableExists("silver.fogo_cruzado.rj_pe"):
     df_fogo_cruzado_concatenado = spark.sql(f"""
                                             SELECT * FROM silver.fogo_cruzado.rj
                                             UNION ALL
-                                            SELECT * FROM silver.fogo_cruzado.pe
+                                            SELECT * EXCEPT (subNeighborhood_id,subNeighborhood_name) FROM silver.fogo_cruzado.pe
                                             """)
     
     df_fogo_cruzado_concatenado.write.format('delta').mode('overwrite').saveAsTable(f'silver.fogo_cruzado.rj_pe')
@@ -13,6 +13,19 @@ else:
         CREATE TABLE silver.fogo_cruzado.rj_pe AS
             SELECT * FROM silver.fogo_cruzado.rj
             UNION ALL
-            SELECT * FROM silver.fogo_cruzado.pe;
+            SELECT * EXCEPT (subNeighborhood_id,subNeighborhood_name) FROM silver.fogo_cruzado.pe ;
     """)
+
+
+# COMMAND ----------
+
+df_rj = spark.table("bronze.fogo_cruzado.rj")
+df_pe = spark.table("bronze.fogo_cruzado.pe")
+
+rj_columns = df_rj.columns
+pe_columns = df_pe.columns
+
+for column in pe_columns:
+   if column not in rj_columns:
+    print(column)
 
