c8ff2556903289176efacab27d4acb8533049907
victor-tuda <victor-tuda>
2025-05-29 12:07:05 +0000

    fix: add except to dataframe from rj

--- src/silver/Silver fogo-cruzado concatenado.py
+++ src/silver/Silver fogo-cruzado concatenado.py
@@ -2,7 +2,7 @@
 # Check if the table exists
 if spark.catalog.tableExists("silver.fogo_cruzado.rj_pe"):
     df_fogo_cruzado_concatenado = spark.sql(f"""
-                                            SELECT * FROM silver.fogo_cruzado.rj
+                                            SELECT * EXCEPT (subNeighborhood) FROM silver.fogo_cruzado.rj
                                             UNION ALL
                                             SELECT * EXCEPT (subNeighborhood_id,subNeighborhood_name) FROM silver.fogo_cruzado.pe
                                             """)
@@ -11,21 +11,8 @@ if spark.catalog.tableExists("silver.fogo_cruzado.rj_pe"):
 else:
     spark.sql(f"""
         CREATE TABLE silver.fogo_cruzado.rj_pe AS
-            SELECT * FROM silver.fogo_cruzado.rj
+            SELECT * EXCEPT (subNeighborhood) FROM silver.fogo_cruzado.rj
             UNION ALL
             SELECT * EXCEPT (subNeighborhood_id,subNeighborhood_name) FROM silver.fogo_cruzado.pe ;
     """)
-
-
-# COMMAND ----------
-
-df_rj = spark.table("bronze.fogo_cruzado.rj")
-df_pe = spark.table("bronze.fogo_cruzado.pe")
-
-rj_columns = df_rj.columns
-pe_columns = df_pe.columns
-
-for column in pe_columns:
-   if column not in rj_columns:
-    print(column)
 
