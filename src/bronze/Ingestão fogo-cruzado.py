74cc6a3f57d5213c519089659f4c8d5b4d2f010f
victor-tuda <victor-tuda>
2025-06-01 20:36:20 +0000

    feat: inserting schema

--- src/bronze/Ingestão fogo-cruzado.py
+++ src/bronze/Ingestão fogo-cruzado.py
@@ -7,8 +7,90 @@ sigla = table_name.upper()
 
 # COMMAND ----------
 
+# DBTITLE 1,Definindo schema
+from pyspark.sql.types import StructType, StructField, StringType, BooleanType, DateType, IntegerType
+
+schema = StructType([
+    StructField("address", StringType(), True),
+    StructField("agentPresence", BooleanType(), True),
+    StructField("date", DateType(), True),
+    StructField("documentNumber", StringType(), True),
+    StructField("id", StringType(), True),
+    StructField("latitude", StringType(), True),
+    StructField("longitude", StringType(), True),
+    StructField("policeAction", BooleanType(), True),
+    StructField("relatedRecord", StringType(), True),
+    StructField("city_id", StringType(), True),
+    StructField("city_name", StringType(), True),
+    StructField("contextInfo_massacre", BooleanType(), True),
+    StructField("contextInfo_policeUnit", StringType(), True),
+    StructField("locality_id", StringType(), True),
+    StructField("locality_name", StringType(), True),
+    StructField("neighborhood_id", StringType(), True),
+    StructField("neighborhood_name", StringType(), True),
+    StructField("region_enabled", BooleanType(), True),
+    StructField("region_id", StringType(), True),
+    StructField("region_region", StringType(), True),
+    StructField("region_state", StringType(), True),
+    StructField("state_id", StringType(), True),
+    StructField("state_name", StringType(), True),
+    StructField("subNeighborhood_id", StringType(), True),
+    StructField("subNeighborhood_name", StringType(), True),
+    StructField("victims_age", IntegerType(), True),
+    StructField("victims_deathDate", DateType(), True),
+    StructField("victims_id", StringType(), True),
+    StructField("victims_occurrenceId", StringType(), True),
+    StructField("victims_personType", StringType(), True),
+    StructField("victims_race", StringType(), True),
+    StructField("victims_situation", StringType(), True),
+    StructField("victims_type", StringType(), True),
+    StructField("victims_unit", StringType(), True),
+    StructField("contextInfo_mainReason_id", StringType(), True),
+    StructField("contextInfo_mainReason_name", StringType(), True),
+    StructField("victims_ageGroup_id", StringType(), True),
+    StructField("victims_ageGroup_name", StringType(), True),
+    StructField("victims_agentPosition_id", StringType(), True),
+    StructField("victims_agentPosition_name", StringType(), True),
+    StructField("victims_agentPosition_type", StringType(), True),
+    StructField("victims_agentStatus_id", StringType(), True),
+    StructField("victims_agentStatus_name", StringType(), True),
+    StructField("victims_agentStatus_type", StringType(), True),
+    StructField("victims_coorporation_id", StringType(), True),
+    StructField("victims_coorporation_name", StringType(), True),
+    StructField("victims_genre_id", StringType(), True),
+    StructField("victims_genre_name", StringType(), True),
+    StructField("victims_partie_id", StringType(), True),
+    StructField("victims_partie_name", StringType(), True),
+    StructField("victims_place_id", StringType(), True),
+    StructField("victims_place_name", StringType(), True),
+    StructField("victims_politicalPosition_id", StringType(), True),
+    StructField("victims_politicalPosition_name", StringType(), True),
+    StructField("victims_politicalPosition_type", StringType(), True),
+    StructField("victims_politicalStatus_id", StringType(), True),
+    StructField("victims_politicalStatus_name", StringType(), True),
+    StructField("victims_politicalStatus_type", StringType(), True),
+    StructField("victims_serviceStatus_id", StringType(), True),
+    StructField("victims_serviceStatus_name", StringType(), True),
+    StructField("victims_serviceStatus_type", StringType(), True),
+    StructField("contextInfo_complementaryReasons_id", StringType(), True),
+    StructField("contextInfo_complementaryReasons_name", StringType(), True),
+    StructField("victims_circumstances_id", StringType(), True),
+    StructField("victims_circumstances_name", StringType(), True),
+    StructField("victims_circumstances_type", StringType(), True),
+    StructField("victims_qualifications_id", StringType(), True),
+    StructField("victims_qualifications_name", StringType(), True),
+    StructField("victims_qualifications_type", StringType(), True),
+    StructField("contextInfo_clippings_id", StringType(), True),
+    StructField("contextInfo_clippings_name", StringType(), True)
+])
+
+# COMMAND ----------
+
 # DBTITLE 1,df_fogo_cruzado formato json
-df_raw = spark.read.format("json").option("multiline", "true").load(f"/Volumes/raw/fogo-cruzado/s3-fogo-cruzado-cdc/{sigla}")
+df_raw = spark.read.format("json") \
+    .option("multiline", "true") \
+    .schema(schema) \
+    .load(f"/Volumes/raw/fogo-cruzado/s3-fogo-cruzado-cdc/{sigla}")
 
 # COMMAND ----------
 
