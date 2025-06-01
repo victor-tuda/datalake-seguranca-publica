15370ff9057290763ff5f993d4a5065501167d4c
victor-tuda <victor-tuda>
2025-06-01 21:31:00 +0000

    fix: merge

--- src/silver/Silver fogo-cruzado concatenado.py
+++ src/silver/Silver fogo-cruzado concatenado.py
@@ -2,17 +2,17 @@
 # Check if the table exists
 if spark.catalog.tableExists("silver.fogo_cruzado.rj_pe"):
     df_fogo_cruzado_concatenado = spark.sql(f"""
-                                            SELECT * EXCEPT (sub_bairro) FROM silver.fogo_cruzado.rj
+                                            SELECT * FROM silver.fogo_cruzado.rj
                                             UNION ALL
-                                            SELECT * EXCEPT (subNeighborhood_id,subNeighborhood_name) FROM silver.fogo_cruzado.pe
+                                            SELECT * FROM silver.fogo_cruzado.pe
                                             """)
     
     df_fogo_cruzado_concatenado.write.format('delta').mode('overwrite').saveAsTable(f'silver.fogo_cruzado.rj_pe')
 else:
     spark.sql(f"""
         CREATE TABLE silver.fogo_cruzado.rj_pe AS
-            SELECT * EXCEPT (sub_bairro) FROM silver.fogo_cruzado.rj
+            SELECT * FROM silver.fogo_cruzado.rj
             UNION ALL
-            SELECT * EXCEPT (subNeighborhood_id,subNeighborhood_name) FROM silver.fogo_cruzado.pe ;
+            SELECT * FROM silver.fogo_cruzado.pe ;
     """)
 
